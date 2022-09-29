c199b83de6f6e6e7e79c09d77d2c8f9b420aea86
ViktorPratama <yohane.e.v.pratama1908@gmail.com>
2022-09-29 14:53:16 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+echo "# Viktor" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ViktorPratama/Viktor.git
+git push -u origin main
+?>
+
+<!DOCTYPE html>
+<head> 
+    <title> Selamat datang
+    </title>
+</head>
+<body> 
+    <h1> Viktor Pratama</h1>
+</body>
+
+</html>
